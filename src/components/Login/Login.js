b06b50996b474e6eb5425b069bd3ac9f8d54c0b6
AnupNair08 <nair.anup09@gmail.com>
2020-07-05 12:16:31 +0530

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SpotifyLogin from "react-spotify-login";
+import SpotifyLogin from "./react-spotify-login/src";
 import { connect } from "react-redux";
 import "./Login.css";
 import { Image } from "react-bootstrap";
